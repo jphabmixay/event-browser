b69954fc0a8f6f25790d5d086012c6b32ba3d85f
Joe Phabmixay <jphabmixay@gmail.com>
2016-12-10 20:55:52 -0800

    -Added Json Class Constructor to Meetup.java -Added beginning of meetup ArrayList to handle json searching in Meetup.java

--- app/src/main/java/csc413team18/finalproject/model/Meetup.java
+++ app/src/main/java/csc413team18/finalproject/model/Meetup.java
@@ -1,5 +1,12 @@
 package csc413team18.finalproject.model;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Model class for Meetup
  */
@@ -11,6 +18,36 @@ public class Meetup {
     private String mAttendies;    // number of attendies
     private String mdescription;
     private String mLink;         // url for meetup
+
+    //Handles list of Meetups as an arraylist for searching
+    public static List<Meetup> parseJson(JSONObject jsonObject) throws JSONException{
+
+        List<Meetup> meetups = new ArrayList<>();
+
+        // Check if the JSONObject has object with key "Search"
+        if(jsonObject.has("Search")){
+
+            // Get JSONArray from JSONObject
+            JSONArray jsonArray = jsonObject.getJSONArray("Search");
+
+            for(int i = 0; i < jsonArray.length(); i++){
+                // Create new Meetup object from each JSONObject in the JSONArray
+                meetups.add(new Meetup(jsonArray.getJSONObject(i)));
+            }
+        }
+
+        //returns list of meetups
+        return meetups;
+    }
+
+    // Json Class constructor
+    private Meetup(JSONObject jsonObject) throws JSONException {
+        if(jsonObject.has("mName")) this.setName(jsonObject.getString("mName"));
+        if(jsonObject.has("mMeetupID")) this.setMeetupId(jsonObject.getString("mMeetupID"));
+        if(jsonObject.has("mAttendies")) this.setAttendies(jsonObject.getString("mAttendies"));
+        if(jsonObject.has("mdescription")) this.setMdescription(jsonObject.getString("mdescription"));
+        if(jsonObject.has("mLink")) this.setLink(jsonObject.getString("mLink"));
+    }
 
 
     public String getMeetupId() {
